de758a63957b2fae269777407cb0a77ee2797462
azizbekqiyomov55555-dev <azizbekqiyomov55555@gmail.com>
2026-03-26 04:00:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,8 +19,8 @@ from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import CommandStart, Command
 
 # ================== SOZLAMALAR ==================
-BOT_TOKEN = "8766647589:AAHmY6x59GgKA25K3e737-7jomufi9wRv2Y"
-ADMIN_ID = 8332077004
+BOT_TOKEN = "8745465963:AAFEOfQ90-2Rb6ok10QMumHoNYfKwmPWZjA"
+ADMIN_ID = 8537782289
 MAIN_CHANNEL_ID = "@Azizbekl2026"
 
 # ================== CHECKOUT.UZ SOZLAMALARI ==================
